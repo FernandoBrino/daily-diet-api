fdd3c26a993afbcb94550baab2ee029612508ac5
Fernando Brino <fernandobrino11@gmail.com>
2023-07-11 12:44:56 -0300

    test: get total meals off and on diet

--- test/diets.spec.ts
+++ test/diets.spec.ts
@@ -72,7 +72,7 @@ describe("Diets routes", () => {
     ]);
   });
 
-  it.only("should get a single user diet", async () => {
+  it("should get a single user diet", async () => {
     const createNewUserResponse = await request(app.server)
       .post("/users")
       .send({
@@ -107,6 +107,136 @@ describe("Diets routes", () => {
         name: "Bulking",
         description: "Comer muito",
         date_hour: "2023-07-10",
+      }),
+    ]);
+  });
+
+  it("should get a total of diets registered", async () => {
+    const createNewUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        name: "John Doe",
+      })
+      .expect(201);
+
+    const cookies = createNewUserResponse.get("Set-Cookie");
+
+    await request(app.server)
+      .post("/diets")
+      .set("Cookie", cookies)
+      .send({
+        name: "Bulking",
+        description: "Comer muito",
+        date_hour: "2023-07-10",
+      })
+      .expect(201);
+
+    await request(app.server)
+      .get("/diets/total-registered")
+      .set("Cookie", cookies)
+      .expect(200, {
+        totalMeals: 1,
+      });
+  });
+
+  it("should get a total of meals on diet", async () => {
+    const createNewUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        name: "John Doe",
+      })
+      .expect(201);
+
+    const cookies = createNewUserResponse.get("Set-Cookie");
+
+    await request(app.server)
+      .post("/diets")
+      .set("Cookie", cookies)
+      .send({
+        name: "Bulking",
+        description: "Comer muito",
+        date_hour: "2023-07-10",
+      })
+      .expect(201);
+
+    const listDietsResponse = await request(app.server)
+      .get("/diets")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    await request(app.server)
+      .put(`/diets/${listDietsResponse.body.diets[0].id}`)
+      .set("Cookie", cookies)
+      .send({
+        name: "Cutting",
+        description: "Comer menos",
+        date_hour: "2023-07-10",
+        is_on_diet: true,
+      })
+      .expect(200);
+
+    const totalOnDietResponse = await request(app.server)
+      .get("/diets/total-on-diet")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(totalOnDietResponse.body.totalOnDiet).toEqual([
+      expect.objectContaining({
+        name: "Cutting",
+        description: "Comer menos",
+        date_hour: "2023-07-10",
+        is_on_diet: 1,
+      }),
+    ]);
+  });
+
+  it("should get a total of meals off diet", async () => {
+    const createNewUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        name: "John Doe",
+      })
+      .expect(201);
+
+    const cookies = createNewUserResponse.get("Set-Cookie");
+
+    await request(app.server)
+      .post("/diets")
+      .set("Cookie", cookies)
+      .send({
+        name: "Bulking",
+        description: "Comer muito",
+        date_hour: "2023-07-10",
+      })
+      .expect(201);
+
+    const listDietsResponse = await request(app.server)
+      .get("/diets")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    await request(app.server)
+      .put(`/diets/${listDietsResponse.body.diets[0].id}`)
+      .set("Cookie", cookies)
+      .send({
+        name: "Cutting",
+        description: "Comer menos",
+        date_hour: "2023-07-10",
+        is_on_diet: false,
+      })
+      .expect(200);
+
+    const totalOnDietResponse = await request(app.server)
+      .get("/diets/total-off-diet")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(totalOnDietResponse.body.totalOnDiet).toEqual([
+      expect.objectContaining({
+        name: "Cutting",
+        description: "Comer menos",
+        date_hour: "2023-07-10",
+        is_on_diet: 0,
       }),
     ]);
   });
